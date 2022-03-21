7789eba23b4119e5b9753ecf7ca240d44c915aac
KotzurPL <kotzur.pl@gmail.com>
2022-03-22 00:02:30 +0100

    InitC

---
+++ src/main/java/pl/kotzur/earek_z2/EarekZ2Application.java
@@ -0,0 +1,13 @@
+package pl.kotzur.earek_z2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarekZ2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarekZ2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kotzur/earek_z2/EarekZ2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kotzur.earek_z2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarekZ2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
